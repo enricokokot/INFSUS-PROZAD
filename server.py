be1bd24fd45b42ed9a4e1d4402b83989987d3f1b
Enrico Kokot <kokotenrico@gmail.com>
2022-05-22 12:02:55 +0200

    Implement database and some basic operations

--- server.py
+++ server.py
@@ -1,6 +1,45 @@
-from flask import Flask, render_template
+from flask import Flask, jsonify, make_response, render_template, request
+from pony import orm
+
+DB = orm.Database()
 
 app = Flask(__name__)
+
+
+class Item(DB.Entity):
+    name = orm.Required(str, unique=True)
+    category = orm.Required(str)
+    price = orm.Required(float)
+
+
+DB.bind(provider="sqlite", filename="database.sqlite", create_db=True)
+DB.generate_mapping(create_tables=True)
+
+
+def add_item(json_request):
+    try:
+        name = json_request["name"]
+        category = json_request["category"]
+        price = json_request["price"]
+        with orm.db_session:
+            Item(name=name, category=category, price=price)
+            response = {"response": "Success"}
+            return response
+    except Exception as e:
+        return {"response": "Fail", "error": e}
+
+
+def get_all_items():
+    try:
+        with orm.db_session:
+            db_querry = orm.select(x for x in Item)[:]
+            results_list = []
+            for r in db_querry:
+                results_list.append(r.to_dict())
+            response = {"response": "Success", "data": results_list}
+            return response
+    except Exception as e:
+        return {"response": "Fail", "error": e}
 
 
 @app.route('/')
@@ -8,24 +47,35 @@ def home():
     return render_template('home.html')
 
 
-@app.route('/register')
+@app.route('/register', methods=["GET"])
 def register():
-    items = [{id: 0, 'name': 'milk', 'category': 'dairy', 'price': 9.99},
-             {id: 1, 'name': 'cheese', 'category': 'dairy', 'price': 4.99},
-             {id: 2, 'name': 'soda', 'category': 'beverage', 'price': 6.99},
-             {id: 3, 'name': 'flour', 'category': 'grain', 'price': 2.99},
-             {id: 4, 'name': 'pork steak', 'category': 'meat', 'price': 24.99},
-             {id: 4, 'name': 'pork steak', 'category': 'meat', 'price': 24.99},
-             {id: 4, 'name': 'pork steak', 'category': 'meat', 'price': 24.99},
-             {id: 4, 'name': 'pork steak', 'category': 'meat', 'price': 24.99},
-             {id: 4, 'name': 'pork steak', 'category': 'meat', 'price': 24.99},
-             {id: 4, 'name': 'pork steak', 'category': 'meat', 'price': 24.99},
-             ]
-    return render_template('register.html', items=items)
+    response = get_all_items()
+    if response["response"] == "Success":
+        return make_response(render_template("register.html", items=response["data"]), 200)
+    else:
+        return make_response(jsonify(response), 400)
 
 
-@app.route('/manager')
+@app.route('/manager', methods=["POST", "GET"])
 def manager():
+    if request.method == "POST":
+        try:
+            json_request = {}
+            for key, value in request.form.items():
+                if value == "":
+                    json_request[key] = None
+                else:
+                    json_request[key] = value
+        except Exception as e:
+            response = {"response": str(e)}
+            return make_response(jsonify(response), 400)
+
+        response = add_item(json_request)
+
+        if response["response"] == "Success":
+            return make_response(render_template("manager.html"), 200)
+        else:
+            return make_response(jsonify(response), 400)
     return render_template('manager.html')
 
 

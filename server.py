a8f0d601effd83b314f37dd2a16481575272a509
Enrico Kokot <kokotenrico@gmail.com>
2022-05-22 09:39:19 +0200

    Set up basic register screen

--- server.py
+++ server.py
@@ -10,7 +10,18 @@ def home():
 
 @app.route('/register')
 def register():
-    return render_template('register.html')
+    items = [{id: 0, 'name': 'milk', 'category': 'dairy', 'price': 9.99},
+             {id: 1, 'name': 'cheese', 'category': 'dairy', 'price': 4.99},
+             {id: 2, 'name': 'soda', 'category': 'beverage', 'price': 6.99},
+             {id: 3, 'name': 'flour', 'category': 'grain', 'price': 2.99},
+             {id: 4, 'name': 'pork steak', 'category': 'meat', 'price': 24.99},
+             {id: 4, 'name': 'pork steak', 'category': 'meat', 'price': 24.99},
+             {id: 4, 'name': 'pork steak', 'category': 'meat', 'price': 24.99},
+             {id: 4, 'name': 'pork steak', 'category': 'meat', 'price': 24.99},
+             {id: 4, 'name': 'pork steak', 'category': 'meat', 'price': 24.99},
+             {id: 4, 'name': 'pork steak', 'category': 'meat', 'price': 24.99},
+             ]
+    return render_template('register.html', items=items)
 
 
 @app.route('/manager')

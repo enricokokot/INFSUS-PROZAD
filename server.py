a40f9344ecda4c53d3ce8bd21439e847c38d5791
Enrico Kokot <kokotenrico@gmail.com>
2022-06-17 09:05:12 +0200

    Introduce daily traffic statistics and graph

--- server.py
+++ server.py
@@ -21,6 +21,11 @@ class Receipt(DB.Entity):
     total_price = orm.Required(float)
 
 
+class DailyTraffic(DB.Entity):
+    date = orm.Required(str)
+    total_traffic = orm.Required(float)
+
+
 DB.bind(provider="sqlite", filename="database.sqlite", create_db=True)
 DB.generate_mapping(create_tables=True)
 
@@ -89,6 +94,7 @@ def sell_items(json_request):
             sold_items = []
             total_cost = 0
             amount_sold = []
+            current_time = datetime.now()
             for item_name, sold_amount in json_request.items():
                 if (int(sold_amount) > 0):
                     item = Item.get(name=item_name)
@@ -96,10 +102,16 @@ def sell_items(json_request):
                     sold_items.append(item.name)
                     amount_sold.append(int(sold_amount))
                     total_cost += int(sold_amount) * item.price
-            Receipt(time=str(datetime.now()),
+            Receipt(time=str(current_time),
                     items=sold_items,
                     amounts=amount_sold,
                     total_price=total_cost)
+            existing_traffic = DailyTraffic.get(date=str(current_time.date()))
+            if (existing_traffic):
+                existing_traffic.total_traffic += total_cost
+            else:
+                DailyTraffic(date=str(current_time.date()),
+                             total_traffic=total_cost)
             db_querry = orm.select(x for x in Item)[:]
             results_list = []
             for r in db_querry:
@@ -129,6 +141,19 @@ def get_all_receipts():
     try:
         with orm.db_session:
             db_querry = orm.select(x for x in Receipt)[:]
+            results_list = []
+            for r in db_querry:
+                results_list.append(r.to_dict())
+            response = {"response": "Success", "data": results_list}
+            return response
+    except Exception as e:
+        return {"response": "Fail", "error": e}
+
+
+def get_all_daily_traffic():
+    try:
+        with orm.db_session:
+            db_querry = orm.select(x for x in DailyTraffic)[:]
             results_list = []
             for r in db_querry:
                 results_list.append(r.to_dict())
@@ -224,8 +249,18 @@ def manager():
 
     else:
         response = get_all_items()
-        if response["response"] == "Success":
-            return make_response(render_template("manager.html", items=response["data"], receipts=get_all_receipts()["data"]), 200)
+        response2 = get_all_receipts()
+        response3 = get_all_daily_traffic()
+        if response["response"] == "Success" and response2["response"] == "Success" and response3["response"] == "Success":
+            items_data = response["data"]
+            receipts_data = response2["data"]
+            daily_traffic = response3["data"]
+            days = []
+            traffic = []
+            for day in daily_traffic[-5:]:
+                days.append(day["date"])
+                traffic.append(day["total_traffic"])
+            return make_response(render_template("manager.html", items=items_data, receipts=receipts_data, days=days, traffic=traffic), 200)
         else:
             return make_response(jsonify(response), 400)
 
